03e4396f66e7235ae81f9e6a7d99a8a27f889bef
KanVull <minirorus2@gmail.com>
2024-01-05 21:30:52 +0400

    Added template files and setup them to django project.

---
+++ store/products/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    """View for the main page."""
+    return render(request, 'products/index.html')
+
+def products(request):
+    """View for products page."""
+    return render(request, 'products/products.html')

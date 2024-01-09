9ae5c0e3442a20306f0c697473ad7fa6c3184d55
KanVull <minirorus2@gmail.com>
2024-01-09 16:15:40 +0400

    Added context and static to html-templates

--- store/products/views.py
+++ store/products/views.py
@@ -3,9 +3,40 @@ from django.shortcuts import render
 
 def index(request):
     """View for the main page."""
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+        'is_promotion': False,
+        'username': 'Roman',
+    }
+    return render(request, 'products/index.html', context=context)
 
 
 def products(request):
     """View for products page."""
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Catalog',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'card_title': 'Black hoodie with monograms adidas Originals',
+                'price': 6090.00,
+                'description': \
+                    'Soft fabric for sweatshirts. Style and comfort are a way of life.',
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'card_title': 'Blue jacket The North Face',
+                'price': 23725.00,
+                'description': \
+                    'Smooth fabric. Waterproof coating. Light and warm down filling.',
+            },
+            {
+                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'card_title': 'Brown sport oversized-top ASOS DESIGN',
+                'price': 3390.00,
+                'description': \
+                    'Plush texture material. Comfortable and soft.',
+            },
+        ],
+    }
+    return render(request, 'products/products.html', context=context)

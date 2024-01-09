b0e63f3d91087b20217cfc85d63fcc83596efb36
KanVull <minirorus2@gmail.com>
2024-01-09 22:24:11 +0400

    Added Product and Categories models and filled up DB.

--- store/core/admin.py
+++ store/core/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
 
-# Register your models here.
+from core.models import (
+    Product,
+    ProductCategory,
+)
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
--- store/core/models.py
+++ store/core/models.py
@@ -1,3 +1,27 @@
-from django.db import models # noqa
+"""Models of store."""
+from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """Model of items category."""
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Model of product - item of store."""
+    name = models.CharField(max_length=128, unique=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return f'Product: {self.name} | Category: {self.category}'

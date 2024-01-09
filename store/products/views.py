5c380c335ae3885f5538167c22193d8c334c7502
KanVull <minirorus2@gmail.com>
2024-01-09 16:21:23 +0400

    Lint checking disable for now.

--- store/products/views.py
+++ store/products/views.py
@@ -20,21 +20,23 @@ def products(request):
                 'image': '/static/vendor/img/products/Adidas-hoodie.png',
                 'card_title': 'Black hoodie with monograms adidas Originals',
                 'price': 6090.00,
-                'description': \
-                    'Soft fabric for sweatshirts. Style and comfort are a way of life.',
+                'description':
+                    'Soft fabric for sweatshirts. Style and comfort \
+                    are a way of life.',
             },
             {
                 'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
                 'card_title': 'Blue jacket The North Face',
                 'price': 23725.00,
-                'description': \
-                    'Smooth fabric. Waterproof coating. Light and warm down filling.',
+                'description':
+                    'Smooth fabric. Waterproof coating. Light and warm \
+                    down filling.',
             },
             {
                 'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
                 'card_title': 'Brown sport oversized-top ASOS DESIGN',
                 'price': 3390.00,
-                'description': \
+                'description':
                     'Plush texture material. Comfortable and soft.',
             },
         ],

c6dee00d81206bec7cf62eb744321c9955af8acb
KanVull <minirorus2@gmail.com>
2024-01-21 20:25:09 +0400

    Added adminpage for user and product

--- store/core/admin.py
+++ store/core/admin.py
@@ -4,9 +4,50 @@ from core.models import (
     Product,
     ProductCategory,
     User,
+    Cart,
 )
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
 
-admin.site.register(User)
+
+class CartAdmin(admin.TabularInline):
+    model = Cart
+    fields = (
+        'product',
+        'quantity',
+        'created_timestamp',
+    )
+    readonly_fields = ('created_timestamp',)
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'category',
+        'quantity'
+    )
+    fields = (
+        'name',
+        'description',
+        (
+            'price',
+            'quantity',
+        ),
+        'image',
+        'category',
+    )
+    readonly_fields = (
+        'quantity',
+    )
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ('username',)
+    inlines = (CartAdmin,)
--- store/core/models.py
+++ store/core/models.py
@@ -8,6 +8,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Product category'
+        verbose_name_plural = 'Product categories'
+
     def __str__(self):
         return self.name
 

c7930b076d7ea87b45c932e47d2c37dd0be0e99c
KanVull <minirorus2@gmail.com>
2024-02-04 19:51:38 +0400

    Fixed flake8 requirements.

--- store/users/tasks.py
+++ store/users/tasks.py
@@ -1,18 +1,19 @@
 import uuid
-from celery import shared_task
-from django.utils.timezone import now
 from datetime import timedelta
 
-from core.models import User, EmailVerification
+from celery import shared_task
+from django.utils.timezone import now
+
+from core.models import EmailVerification, User
 
 
 @shared_task
 def send_email_verification(user_id):
-        user = User.objects.get(id=user_id)
-        expiration = now() + timedelta(hours=24)
-        record = EmailVerification.objects.create(
-            code=uuid.uuid4(),
-            user=user,
-            expiration=expiration,
-        )
-        record.send_email_verification()
+    user = User.objects.get(id=user_id)
+    expiration = now() + timedelta(hours=24)
+    record = EmailVerification.objects.create(
+        code=uuid.uuid4(),
+        user=user,
+        expiration=expiration,
+    )
+    record.send_email_verification()

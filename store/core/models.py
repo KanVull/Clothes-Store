48b993d85486e9823c7a735a95cc0f67d8a14389
KanVull <minirorus2@gmail.com>
2024-02-20 18:07:16 +0400

    Create Order model and migration file

--- store/core/models.py
+++ store/core/models.py
@@ -97,3 +97,31 @@ class EmailVerification(models.Model):
 
     def __str__(self) -> str:
         return f'EmailVerification object for {self.user.email}'
+
+
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, 'Created'),
+        (PAID, 'Paid'),
+        (ON_WAY, 'Deliver'),
+        (DELIVERED, 'Delivered'),
+    )
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=256)
+    address = models.CharField(max_length=256)
+    cart_history = models.JSONField(default=dict)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.SmallIntegerField(default=CREATED, choices=STATUSES)
+    initiator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'''
+            Order #{self.id}. {self.first_name} {self.last_name} 
+            | {self.created}
+        '''
